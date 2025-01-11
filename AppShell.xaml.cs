d6470964d500dbb02cb1e2fce17ff11bcc91e026
IbrahimSchi6 <ibrahimschi6@gmail.com>
2025-01-11 08:52:29 +0100

    Add Hamburger Menu

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -5,5 +5,7 @@ public partial class AppShell : Shell
 	public AppShell()
 	{
 		InitializeComponent();
+		    this.FlyoutBehavior = FlyoutBehavior.Flyout; // Ensures hamburger menu is shown
+
 	}
 }
